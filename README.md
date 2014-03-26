5d33bd5c8aeb6a9a952b922a7c7f5b8919ee4c31
Tim Kurvers <tim@moonsphere.net>
2014-03-26 20:48:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Heroku Pages
+
+Easily inspect, view and upload your [Heroku error and maintenance pages](https://devcenter.heroku.com/articles/error-pages#customize-pages).
+
+**Supported Ruby versions: 1.9.3 or higher**
+
+Licensed under the **MIT** license, see LICENSE for more information.
+
+
+## Installation
+
+This is a Heroku client plugin and as such requires the [Heroku Toolbelt](https://toolbelt.heroku.com/) to be installed.
+
+```shell
+heroku plugins:install https://github.com/hyperoslo/heroku-pages.git
+```
+
+
+## Usage
+
+If you have multiple apps, you may specify either app or remote:
+
+```shell
+heroku pages -a hyper-rocks-staging
+heroku pages -r staging
+```
+
+### Inspecting
+
+To view the current set of Heroku pages:
+
+```shell
+heroku pages
+```
+
+### Opening / Viewing
+
+To open all Heroku pages using the default application (which tends to be a browser):
+
+```shell
+heroku pages:open
+```
+
+### Uploading to S3
+
+To upload your Heroku pages to S3 using [S3 Uploader](https://github.com/chrishein/s3_uploader):
+
+```shell
+heroku pages:upload
+```
+
+It assumes the following configuration variables to be present for the given app:
+
+- `AWS_REGION`
+- `AWS_BUCKET`
+- `AWS_KEY` or `AWS_ACCESS_KEY_ID`
+- `AWS_SECRET` or `AWS_SECRET_ACCESS_KEY`
+
+And it assumes presence of two folders:
+
+- Local folder `public/heroku_pages` in your project
+- Remote folder `heroku_pages` on your S3 bucket
+
+All files within the local Heroku pages folder will be made public.
+
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create pull request
+
+
+## Credits
+
+Hyper made this. We're a digital communications agency with a passion for good code,
+and if you're using this plugin we probably want to hire you.
---
+++ lib/heroku/command/pages.rb
@@ -0,0 +1,102 @@
+require 'heroku/command/base'
+
+require 'pty'
+
+# handles Heroku's error and maintenance pages
+class Heroku::Command::Pages < Heroku::Command::Base
+
+  PAGES = {
+    error:       'ERROR_PAGE_URL',
+    maintenance: 'MAINTENANCE_PAGE_URL'
+  }
+
+  FOLDER = 'heroku_pages'
+
+  LOCAL_FOLDER  = "public/#{FOLDER}"
+  REMOTE_FOLDER = FOLDER
+
+  # pages
+  #
+  #   Lists current Heroku pages
+  #
+  def index
+    PAGES.each do |page, variable|
+      display "#{page}: #{config[variable]}"
+    end
+  end
+
+  # pages:open
+  #
+  #   Opens all Heroku pages in default application (may be OSX only)
+  #
+  def open
+    PAGES.each do |page, variable|
+      shell("open #{config[variable]}")
+    end
+  end
+
+  # pages:upload
+  #
+  #   Uploads Heroku pages to an S3 bucket
+  #
+  def upload
+    extract_and_verify_credentials
+
+    run('s3uploader --help &>/dev/null')
+    unless $?.success?
+      error('Ensure `s3uploader` binary is available (`gem install s3_uploader`)')
+    end
+
+    run("s3uploader -p true -r #{@region} -k #{@key} -s #{@secret} -d #{REMOTE_FOLDER} #{LOCAL_FOLDER} #{@bucket}")
+
+    if $?.success?
+      display("\e[92mUpload successful.\e[0m")
+    else
+      error("\e[91m\e[5mUpload failed.\e[0m")
+    end
+  end
+
+  private
+
+  # Lazily loads app's configuration variables
+  def config
+    @config ||= api.get_config_vars(app).body
+  end
+
+  # Extracts and verifies credentials from app's configuration variables
+  def extract_and_verify_credentials
+    @bucket = config['AWS_BUCKET']
+    @region = config['AWS_REGION']
+    @key    = config['AWS_KEY'] || config['AWS_ACCESS_KEY_ID']
+    @secret = config['AWS_SECRET'] || config['AWS_SECRET_ACCESS_KEY']
+
+    unless @bucket || @region || @key || @secret
+      error('Ensure AWS_BUCKET, AWS_REGION, AWS_KEY and AWS_SECRET are set')
+    end
+  end
+
+  # Runs given command using a pseudo-terminal to allow streaming of output
+  #
+  # Options:
+  #   :out (io, defaults to $stdout)
+  #
+  def run(command, options = {})
+    options = options.merge!(
+      out: $stdout
+    )
+
+    buffer = StringIO.new
+
+    PTY.spawn(command) do |output, input, pid|
+      while !output.eof?
+        chunk = output.readpartial(1024)
+        buffer << chunk
+        options[:out].print(chunk) if options[:out]
+      end
+      Process.wait(pid)
+    end
+
+    buffer.string
+  end
+
+end
